18ee8d836bc213eeae558e0f76c0741656d15caf
tt6-mzn <tt6-mzn@users.noreply.github.com>
2021-07-19 17:46:36 +0900

    ABC198-C

--- ABC/198/c.cpp
+++ ABC/198/c.cpp
@@ -3,12 +3,13 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-    long long n;
-    cin >> n;
-    int i = 1, ans = 0;
-    while (stoll(to_string(i) + to_string(i)) <= n) {
-        i++;
-        ans++;
+    long long r, x, y;
+    cin >> r >> x >> y;
+    long long d2 = x * x + y * y;
+    if (d2 == r * r) cout << 1 << endl;
+    else if (d2 <= 4 * r * r) cout << 2 << endl;
+    else {
+        double d = sqrt((double)(x * x + y * y));
+        cout << ceil(d / r) << endl;
     }
-    cout << ans << endl;
 }
