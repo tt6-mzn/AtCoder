665d862377ad32da221cc4c54a4db3c1c8cefe74
tt6-mzn <tt6-mzn@users.noreply.github.com>
2021-08-29 18:47:02 +0900

    精進ABC207-B

--- practice/ABC/207/B/main.cpp
+++ practice/ABC/207/B/main.cpp
@@ -5,13 +5,13 @@ using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-	int a, b, c, d;
+	long long a, b, c, d;
 	cin >> a >> b >> c >> d;
-	if (b >= c) {
+	if (b >= c * d) {
 		cout << -1 << endl;
 		return 0;
 	}
-	int n = 0;
+	long long n = 0;
 	while (a + n * b > d * n * c) n++;
 	cout << n << endl;
 }
