10dca25f139d286af0112e8d3d58f58558c8d1aa
tt6-mzn <tt6-mzn@users.noreply.github.com>
2021-09-26 23:34:10 +0900

    ABC220

--- Contest/ABC/220/B/main.cpp
+++ Contest/ABC/220/B/main.cpp
@@ -4,41 +4,10 @@ using namespace std;
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-/*
-int ktod(int x, int k) {
-	int ret = 0;
-	int b = 1;
-	while (x > 0) {
-		ret += b * (x % 10);
-		x /= 10;
-		b *= k;
-	}
-	return ret;
-}
-*/
-
-int ktod(string s, int k) {
-	int ret = 0;
-	int b = 1;
-	for (int i = s.size() - 1; i >= 0; i--) {
-		ret += b * (s[i] - '0');
-		b *= k;
-	}
-	return ret;
-}
-
 int main() {
 	int k;
 	cin >> k;
 	string a, b;
 	cin >> a >> b;
 	cout << (long long)stoi(a, nullptr, k) * (long long)stoi(b, nullptr, k) << endl;
-
-	/*
-	for (int i = 1; i <= 10; i++) {
-		string x;
-		cin >> x;
-		cout << ktod(x, k) << endl;
-	}
-	*/
 }
