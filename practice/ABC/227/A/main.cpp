8bf6e69df631193087b164996472084cc1fff7d6
tt6-mzn <tt6-mzn@users.noreply.github.com>
2021-11-14 19:57:52 +0900

    精進ABC227

--- practice/ABC/227/A/main.cpp
+++ practice/ABC/227/A/main.cpp
@@ -5,12 +5,9 @@ using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
 int main() {
-	int x;
-	cin >> x;
-	if (x == 0) {
-		cout << "No" << endl;
-		return 0;
-	}
-	if (x % 100 == 0) cout << "Yes" << endl;
-	else cout << "No" << endl;
+    int n, k, a;
+    cin >> n >> k >> a;
+    int ans = (a + k - 1) % n;
+    if (ans == 0) cout << n << endl;
+    else cout << ans << endl;
 }
