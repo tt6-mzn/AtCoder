edf07047ec17b6aa580efd081369ee255e32a014
tt6-mzn <tt6-mzn@users.noreply.github.com>
2022-03-18 04:40:02 +0900

    クラスの修正と名称の変更

--- mylib/dijkstra.cpp
+++ mylib/dijkstra.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 
 const long long INF = 1LL << 62;
 
 class Edge {
 public:
-	int from; // 辺の始点
 	int to; // 辺の終点
 	long long cost = 1; // 辺の重み
 	Edge() {};
-	Edge(int from, int to) : from(from), to(to) {};
-	Edge(int from, int to, long long cost)
-		: from(from), to(to), cost(cost) {};
+	Edge(int to) : to(to) {};
+	Edge(int to, long long cost) : to(to), cost(cost) {};
 };
 
-class Graph {
+class Dijkstra {
 private:
 	vector<vector<Edge>> _Graph; // _Graph[i]:=頂点iから出る辺
 	vector<long long> _distance; // dijkstra()の結果が格納される
@@ -23,7 +22,7 @@ private:
 public:
 	int V; // 頂点数
 	int E = 0; // 辺数
-	Graph(int V);
+	Dijkstra(int V);
 	void add_edge(int from, int to);
 	void add_edge(int from, int to, long long cost);
 	void dijkstra(int s); // ダイクストラ法
@@ -32,11 +31,11 @@ public:
 };
 
 /* 頂点数と辺の数を指定して初期化 */
-Graph::Graph(int V) : V(V) {
+Dijkstra::Dijkstra(int V) : V(V) {
 	_Graph.resize(V);
 }
 
-void Graph::add_edge(int from, int to) {
+void Dijkstra::add_edge(int from, int to) {
 	if (from < 0 || V <= from) {
 		throw out_of_range("[Graph::add_edge] index out of range");
 		exit(0);
@@ -45,12 +44,12 @@ void Graph::add_edge(int from, int to) {
 		throw out_of_range("[Graph::add_edge] index out of range");
 		exit(0);
 	}
-	Edge tmp(from, to);
+	Edge tmp(to);
 	_Graph.at(from).push_back(tmp);
 	E++;
 }
 
-void Graph::add_edge(int from, int to, long long cost) {
+void Dijkstra::add_edge(int from, int to, long long cost) {
 	if (from < 0 || V <= from) {
 		throw out_of_range("[Graph::add_edge] index out of range");
 		exit(0);
@@ -59,7 +58,7 @@ void Graph::add_edge(int from, int to, long long cost) {
 		throw out_of_range("[Graph::add_edge] index out of range");
 		exit(0);
 	}
-	Edge tmp(from, to, cost);
+	Edge tmp(to, cost);
 	_Graph.at(from).push_back(tmp);
 	E++;
 }
@@ -68,51 +67,51 @@ void Graph::add_edge(int from, int to, long long cost) {
  * 計算量はO(ElogV)
  * 返り値: 各頂点への最短距離が入った配列
 */
-void Graph::dijkstra(int s) {
+void Dijkstra::dijkstra(int s) {
 	_distance.assign(V, INF);
 	_prev.assign(V, -1);
 	priority_queue<
-		pair<long long, int>,
-		vector<pair<long long, int>>,
-		greater<pair<long long, int>>> que;
-	_distance.at(s) = 0;
-	que.emplace(_distance.at(s), s);
+		tuple<long long, int, int>,
+		vector<tuple<long long, int, int>>,
+		greater<tuple<long long, int, int>>> que;
+	que.emplace(0, -1, s);
 	while (!que.empty()) {
-		long long d; int v;
-		tie(d, v) = que.top(); que.pop();
-		if (_distance.at(v) < d) continue;
-		for (const Edge& e : _Graph.at(v)) {
-			if (_distance.at(e.from) + e.cost < _distance.at(e.to)) {
-				_distance.at(e.to) = _distance.at(e.from) + e.cost;
-				_prev.at(e.to) = e.from;
-				que.emplace(_distance.at(e.to), e.to);
-			}
+		long long d; int pre, v;
+		tie(d, pre, v) = que.top(); que.pop();
+		if (_distance[v] != INF) continue;
+		_distance[v] = d;
+		_prev[v] = pre;
+		for (const Edge& next : _Graph[v]) {
+			que.emplace(d + next.cost, v, next.to);
 		}
 	}
 }
 
-long long Graph::distance(int to) const {
-	if (to < 0 || V <= to) {
+/* dijkstra実行後にのみ使用可能
+ * 頂点toまでの最短距離を返す
+*/
+long long Dijkstra::distance(int to) const {
+	if (_distance.size() == 0 || to < 0 || V <= to) {
 		throw out_of_range("[Graph::distance] index out of range");
+		exit(0);
 	}
 	return _distance.at(to);
 }
 
-int main() {
-	int n;
-	cin >> n;
-	Graph G(n + 1);
-	for (int i = 1; i <= n; i++) {
-		int s;
-		cin >> s;
-		if (i < n) G.add_edge(i, i + 1, s);
-		else G.add_edge(i, 1, s);
+/* dijkstra実行後にのみ使用可能
+ * 頂点toまでの最短経路の一つを復元する
+ * 始点の頂点は返り値に含まれない
+*/
+vector<int> Dijkstra::path(int to) const {
+	if (_distance.size() == 0 || to < 0 || V <= to) {
+		throw out_of_range("[Graph::path] index out of range");
+		exit(0);
 	}
-	for (int i = 1; i <= n; i++) {
-		int t;
-		cin >> t;
-		G.add_edge(0, i, t);
+	vector<int> ret;
+	while (_prev.at(to) != -1) {
+		ret.push_back(to);
+		to = _prev.at(to);
 	}
-	G.dijkstra(0);
-	for (int i = 1; i <= n; i++) cout << G.distance(i) << endl;
+	reverse(ret.begin(), ret.end());
+	return ret;
 }
