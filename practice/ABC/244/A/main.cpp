6db4fd3e3d71b9d557fe9ab92f9133f4ae8c1e27
tt6-mzn <tt6-mzn@users.noreply.github.com>
2022-03-21 19:59:59 +0900

    精進ABC244

---
+++ practice/ABC/244/A/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+	int n;
+	cin >> n;
+	string s;
+	cin >> s;
+	cout << s[s.size() - 1] << endl;
+	return 0;
+}
