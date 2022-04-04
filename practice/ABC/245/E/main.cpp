541e990a28175eadfaa1c4d7d3df9896b4523962
tt6-mzn <tt6-mzn@users.noreply.github.com>
2022-04-04 17:35:37 +0900

    精進ABC245-E

--- practice/ABC/245/E/main.cpp
+++ practice/ABC/245/E/main.cpp
@@ -4,47 +4,48 @@ using namespace std;
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
+bool comp(const tuple<int, int, int>& left, const tuple<int, int, int>& right) {
+	int l0, l1, l2, r0, r1, r2;
+	tie(l0, l1, l2) = left;
+	tie(r0, r1, r2) = right;
+	if (l0 != r0) return l0 > r0; // 縦の長さ
+	if (l2 != r2) return l2 > r2; // 箱を優先
+	return l1 > r1; // 横の長さ
+}
+
 int main() {
 	int n, m;
 	cin >> n >> m;
 	vector<int> a(n), b(n), c(m), d(m);
 	rep(i, n) cin >> a[i];
 	rep(i, n) cin >> b[i];
-	rep(i, m) cin >> c[i];
-	rep(i, m) cin >> d[i];
-	vector<tuple<int, int, int>> p;
-	rep(i, n) p.emplace_back(a[i], 0, b[i]); // チョコ
-	rep(i, m) p.emplace_back(c[i], 1, d[i]); // 箱
-	sort(p.begin(), p.end(), greater<tuple<int, int, int>>());
+	rep(j, m) cin >> c[j];
+	rep(j, m) cin >> d[j];
+	vector<tuple<int, int, int>> p(n + m);
+	rep(i, n) p[i] = make_tuple(a[i], b[i], 0); // チョコ
+	rep(j, m) p[n + j] = make_tuple(c[j], d[j], 1); // 箱
+	sort(p.begin(), p.end(), comp);
 
 	// rep(i, n + m) {
-	// 	int a, t, b
-	// 	tie(a, t, b) = p[i];
+	// 	int a, b, t; tie(a, b, t) = p[i];
 	// 	cout << a << " ";
 	// 	cout << b << " ";
 	// 	cout << t << endl;
 	// }
-	
-	multiset<int> mset;
+	// return 0;
+
+	multiset<int> ms;
 	rep(i, n + m) {
-		int a, t, b;
-		tie(a, t, b) = p[i];
-
-		// cout << a << " ";
-		// cout << b << " ";
-		// cout << t << endl;
-
-		if (t == 1) {
-			mset.insert(b);
-		}
+		int a, b, t; tie(a, b, t) = p[i];
 		if (t == 0) {
-			auto it = mset.lower_bound(b);
-			if (it == mset.end()) {
+			auto it = ms.lower_bound(b);
+			if (it == ms.end()) {
 				cout << "No" << endl;
 				return 0;
 			}
-			mset.erase(*it);
+			ms.erase(it);
 		}
+		if (t == 1) ms.insert(b);
 	}
 	cout << "Yes" << endl;
 	return 0;
