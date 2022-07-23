9e1f0310cc873098a0a6a0045d51c8c1d04312e4
tt6-mzn <tt6-mzn@users.noreply.github.com>
2022-07-24 02:05:14 +0900

    ABC261-E

--- Contest/ABC/261/E/main.cpp
+++ Contest/ABC/261/E/main.cpp
@@ -4,11 +4,21 @@ using namespace std;
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-int calc(int x, int op, int y) {
-	if (op == 1) return x & y;
-	else if (op == 2) return x | y;
-	else return x ^ y;
-}
-
 int main() {
+	int n, c;
+	cin >> n >> c;
+	vector<int> f(2);
+	f[0] = 0;
+	f[1] = ~0;
+	rep(i, n) {
+		int t, a;
+		cin >> t >> a;
+		rep(j, 2) {
+			if (t == 1) f[j] &= a;
+			if (t == 2) f[j] |= a;
+			if (t == 3) f[j] ^= a;
+		}
+		c = (c & f[1]) | (~c & f[0]);
+		cout << c << endl;
+	}
 }
