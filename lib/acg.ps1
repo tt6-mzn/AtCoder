6f7bb37921a6ec7d3083e6b908886c95db948841
tt6-mzn <tt6-mzn@users.noreply.github.com>
2022-08-03 23:59:58 +0900

    コンパイラオプションを調整

--- lib/acg.ps1
+++ lib/acg.ps1
@@ -1,1 +1,3 @@
-g++ $Args[0] -std=c++14 -I C:\include\ac-library
+# g++ $Args[0] -std=c++14 -I C:\include\ac-library
+# g++ -std=gnu++1y -O2 -I/opt/boost/gcc/include -IC:\include\ac-library -L/opt/boost/gcc/lib $Args[0]
+g++ -std=c++14 -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -IC:\include\ac-library $Args[0]
