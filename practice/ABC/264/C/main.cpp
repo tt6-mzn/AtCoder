f725d638d214541de3d51f93e24cbb560ed1a1fa
tt6-mzn <tt6-mzn@users.noreply.github.com>
2022-09-25 17:54:45 +0900

    精進ABC264-C

--- practice/ABC/264/C/main.cpp
+++ practice/ABC/264/C/main.cpp
@@ -4,56 +4,35 @@ using namespace std;
 using namespace atcoder;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-template <typename T> bool next_combination(const T first, const T last, int k) {
-    const T subset = first + k;
-    // empty container | k = 0 | k == n 
-    if (first == last || first == subset || last == subset) {
-        return false;
-    }
-    T src = subset;
-    while (first != src) {
-        src--;
-        if (*src < *(last - 1)) {
-            T dest = subset;
-            while (*src >= *dest) {
-                dest++;
-            }
-            iter_swap(src, dest);
-            rotate(src + 1, dest + 1, last);
-            rotate(subset, subset + (last - dest) - 1, last);
-            return true;
-        }
-    }
-    // restore
-    rotate(first, subset, last);
-    return false;
-}
-
 int main() {
-	int h1, w1; cin >> h1 >> w1;
+	int h1, w1;
+	cin >> h1 >> w1;
 	vector<vector<int>> a(h1, vector<int>(w1));
 	rep(i, h1) rep(j, w1) cin >> a[i][j];
-	int h2, w2; cin >> h2 >> w2;
+
+	int h2, w2;
+	cin >> h2 >> w2;
 	vector<vector<int>> b(h2, vector<int>(w2));
 	rep(i, h2) rep(j, w2) cin >> b[i][j];
+
 	if (h1 < h2 || w1 < w2) {
 		cout << "No" << endl;
 		return 0;
 	}
-	vector<int> hc(h1);
-	rep(i, h1) hc[i] = i;
-	vector<int> wc(w1);
-	rep(i, w1) wc[i] = i;
-	while (next_combination(hc.begin(), hc.end(), h2)) {
-		while (next_combination(wc.begin(), wc.end(), w2)) {
-			bool flg = true;
-			rep(i, h2) rep(j, w2) {
-				if (a[hc[i]][wc[j]] != b[i][j]) flg = false;
-			}
-			if (flg) {
-				cout << "Yes" << endl;
-				return 0;
-			}
+
+	rep(bh1, 1 << h1) rep(bw1, 1 << w1) {
+		vector<int> h1ind, w1ind;
+		rep(i, h1) if ((bh1 >> i) & 1) h1ind.push_back(i);
+		rep(j, w1) if ((bw1 >> j) & 1) w1ind.push_back(j); 
+		if (h1ind.size() != h2) continue;
+		if (w1ind.size() != w2) continue;
+		bool flg = true;
+		rep(i, h2) rep(j, w2) {
+			if (a[h1ind[i]][w1ind[j]] != b[i][j]) flg = false;
+		}
+		if (flg) {
+			cout << "Yes" << endl;
+			return 0;
 		}
 	}
 	cout << "No" << endl;
