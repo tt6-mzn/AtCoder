26970dd21a3ec6b6f57edc74eb98cf34ddf9fb3a
tt6-mzn <tt6-mzn@users.noreply.github.com>
2023-03-08 19:21:41 +0900

    include pathを変更

--- lib/acg.ps1
+++ lib/acg.ps1
@@ -1,3 +1,3 @@
 # g++ $Args[0] -std=c++14 -I C:\include\ac-library
-g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -IC:\include\ac-library $Args[0]
+g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -IC:\MyProgram\ac-library $Args[0]
 # g++ -std=c++14 -O2 -DONLINE_JUDGE -I/opt/boost/gcc/include -L/opt/boost/gcc/lib -IC:\include\ac-library $Args[0]
